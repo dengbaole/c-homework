226e096b02698120aef866944ce28114ced5594b
dengbaole <1550555930@qq.com>
2021-07-20 18:25:11 +0800

    do witch和switch显示菜单

--- c.c
+++ c.c
@@ -3,6 +3,79 @@
 #include <stdio.h>
 
 
+
+/*
+    名称：switch语句的应用
+    功能：do witch和switch显示菜单
+    作者：邓宝乐
+    日期：20210720
+*/
+
+
+void menu()
+{
+    printf("*****************************************\n");
+    printf("****** 1. add           2. del    *******\n");
+    printf("****** 3. search        4. modify *******\n");
+    printf("****** 5. show          6. sort   *******\n");
+    printf("****** 0. exit                    *******\n");
+    printf("*****************************************\n");
+}
+
+
+
+int main()
+{
+    int input;
+    do
+    {
+        menu();
+        printf("请输入你的选择：");
+        scanf("%d", &input);
+        switch (input)
+        {
+        case 1:
+            printf("add");
+            break;
+        case 2:
+            printf("del");
+            break;
+        case 3:
+            printf("search");
+            break;
+        case 4:
+            printf("modify");
+            break;
+        case 5:
+            printf("show");
+            break;
+        case 6:
+            printf("sort");
+            break;
+        default:
+            printf("输入错误");
+            break;
+        }
+
+    } while (input)
+            ;
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*
     名称：if语句的应用
     功能：if语句的一般应用
@@ -11,20 +84,20 @@
 
 */
 
-int main()
-{
-    int i = 1;
-    if (i == 1)
-    {
-        printf("%d\n", i);
-    }
-    else
-    {
-        printf("%d\n", i);
-    }
-
-    return 0;
-}
+//int main()
+//{
+//    int i = 1;
+//    if (i == 1)
+//    {
+//        printf("%d\n", i);
+//    }
+//    else
+//    {
+//        printf("%d\n", i);
+//    }
+//
+//    return 0;
+//}
 
 
 
